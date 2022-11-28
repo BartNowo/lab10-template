2d11bc4153af4fd3c492c6cf632985b2ec3174ef
BartNowo <112428609+BartNowo@users.noreply.github.com>
2022-11-28 14:07:35 -0600

    Update locksync.c

--- locksync.c
+++ locksync.c
@@ -23,57 +23,57 @@ void scheduler_init() {
     pthread_mutex_lock(&elevator_lock);
 }
 
-#ifndef USECONDITION
-/********************************************************************************************************************
-Method1: Implementation using busy poll
-********************************************************************************************************************/
-void passenger_request(int passenger, int from_floor, int to_floor, void (*enter)(int, int), void(*exit)(int, int)) {
-    pthread_mutex_lock(&passenger_lock);
+// #ifndef USECONDITION
+// /********************************************************************************************************************
+// Method1: Implementation using busy poll
+// ********************************************************************************************************************/
+// void passenger_request(int passenger, int from_floor, int to_floor, void (*enter)(int, int), void(*exit)(int, int)) {
+//     pthread_mutex_lock(&passenger_lock);
 
-    while(1) {
-        pthread_mutex_lock(&elevator_lock);
-        if(elevator_floor == from_floor) { 
-            enter(passenger,0);
-            pthread_mutex_unlock(&elevator_lock);
-            break;
-        }
-        pthread_mutex_unlock(&elevator_lock);
-    }
+//     while(1) {
+//         pthread_mutex_lock(&elevator_lock);
+//         if(elevator_floor == from_floor) { 
+//             enter(passenger,0);
+//             pthread_mutex_unlock(&elevator_lock);
+//             break;
+//         }
+//         pthread_mutex_unlock(&elevator_lock);
+//     }
 
-    while(1) {
-        pthread_mutex_lock(&elevator_lock);
-        if(elevator_floor == to_floor) { 
-            exit(passenger,0);
-            pthread_mutex_unlock(&elevator_lock);
-            break;
-        }
-        pthread_mutex_unlock(&elevator_lock);
-    }
+//     while(1) {
+//         pthread_mutex_lock(&elevator_lock);
+//         if(elevator_floor == to_floor) { 
+//             exit(passenger,0);
+//             pthread_mutex_unlock(&elevator_lock);
+//             break;
+//         }
+//         pthread_mutex_unlock(&elevator_lock);
+//     }
 
-    pthread_mutex_unlock(&passenger_lock);
-}
+//     pthread_mutex_unlock(&passenger_lock);
+// }
 
-void elevator_ready(int elevator, int at_floor, void(*move_direction)(int, int), void(*door_open)(int), void(*door_close)(int)) {
-    if(elevator == 0) {
-        if(at_floor == FLOORS-1)
-            elevator_direction = -1;
-        if(at_floor == 0)  
-            elevator_direction = 1;
+// void elevator_ready(int elevator, int at_floor, void(*move_direction)(int, int), void(*door_open)(int), void(*door_close)(int)) {
+//     if(elevator == 0) {
+//         if(at_floor == FLOORS-1)
+//             elevator_direction = -1;
+//         if(at_floor == 0)  
+//             elevator_direction = 1;
 
-        door_open(elevator);
+//         door_open(elevator);
 
-        pthread_mutex_unlock(&elevator_lock);
-        usleep(1);
-        pthread_mutex_lock(&elevator_lock);
+//         pthread_mutex_unlock(&elevator_lock);
+//         usleep(1);
+//         pthread_mutex_lock(&elevator_lock);
 
-        door_close(elevator);      
+//         door_close(elevator);      
           
-        move_direction(elevator,elevator_direction);
-        elevator_floor = at_floor+elevator_direction;
-    }
-}
+//         move_direction(elevator,elevator_direction);
+//         elevator_floor = at_floor+elevator_direction;
+//     }
+// }
 
-#else
+// #else
 
 /********************************************************************************************************************
 Method2: Implementation using conditions
@@ -92,7 +92,7 @@ void passenger_request(int passenger, int from_floor, int to_floor, void (*enter
     // TODO 1b: Submit request to elevator by setting 'wait_at'.
     // 'wait_at' is used to store the next floor at which the elevator should stop
     // at. Replace 0 below with the correct value for this request.
-    wait_at = 0;
+    wait_at = from_floor;
 
     // TODO 1c:
     // After setting the 'wait_at' variable, we wait for the elevator to arrive at the
@@ -102,8 +102,8 @@ void passenger_request(int passenger, int from_floor, int to_floor, void (*enter
     // and the elevator sets the variable and signals this thread
     //
     // Fill in the while loop below to wait for the condition to be met.
-    while(0 /* FILL IN CORRECT CONDITION HERE */) {
-        // pthread_cond_wait(_, _);
+    while(is_elevator_ready == 0) {
+        pthread_cond_wait(&elevator_signal, &elevator_lock);
     }
     is_elevator_ready = 0;
 
@@ -115,7 +115,10 @@ void passenger_request(int passenger, int from_floor, int to_floor, void (*enter
     // that they have entered. As in the previous TODOs, use pthread_cond_signal to notify
     // the elevator thread that the passenger is ready.
     /* FILL IN HERE */
-    pthread_mutex_unlock(&elevator_lock);
+    is_passenger_ready=1;		    
+    pthread_cond_signal(&passenger_signal);		
+    wait_at = to_floor;
+    
 
 
     // *************
@@ -123,16 +126,16 @@ void passenger_request(int passenger, int from_floor, int to_floor, void (*enter
     // *************
     // TODO 2: Use what you've learned in the previous TODOs to replace the below busy poll
     // with the condition algorithm.
-    while(1) {
-        pthread_mutex_lock(&elevator_lock);
-        if(elevator_floor == to_floor) { 
-            exit(passenger,0);
-            pthread_mutex_unlock(&elevator_lock);
-            break;
-        }
-        pthread_mutex_unlock(&elevator_lock);
+    while(is_elevator_ready == 0) {
+        pthread_cond_wait(&elevator_signal, &elevator_lock);
     }
-
+    
+    is_elevator_ready=0;		
+    exit(passenger,0);		
+    is_passenger_ready=1;		
+    pthread_cond_signal(&passenger_signal);		
+	
+    
     pthread_mutex_unlock(&passenger_lock);
 }
 
@@ -143,12 +146,12 @@ void elevator_ready(int elevator, int at_floor, void(*move_direction)(int, int),
         if(at_floor == 0)  
             elevator_direction = 1;
 
-        door_open(elevator);
 
         if (wait_at == at_floor) {
             // There is a passenger waiting at this floor.
             // We set wait_at back to its default value so it can be used later.
-            wait_at = -1;
+            door_open(elevator);
+            wait_at = 0;
 
             // TODO 1d:
             // The passenger thread waiting for the elevator will be waiting for the elevator
@@ -157,13 +160,17 @@ void elevator_ready(int elevator, int at_floor, void(*move_direction)(int, int),
             //
             // Signal the passenger thread using 'is_elevator_ready', 'elevator_signal' and pthread_cond_signal
             /* FILL IN CODE HERE */
+            is_elevator_ready=1;		
+	        pthread_mutex_unlock(&elevator_lock);		
+	        pthread_cond_signal(&elevator_signal);
 
             // TODO 1f:
             // It's now the passengers turn to notify the elevator thread once it's entered.
             // Wait for the 'is_passenger_ready' condition to be ready. Use the 'passenger_signal'
             // variable along with pthread_cond_wait to wait.
-            while(0 /* FILL IN CORRECT CONDITION HERE */) {
+            while(is_passenger_ready==0) {
                /* WAIT FOR CONDITION HERE */
+               pthread_cond_wait(&passenger_signal,&elevator_lock);
             }
             is_passenger_ready = 0;
         } else {
@@ -175,10 +182,8 @@ void elevator_ready(int elevator, int at_floor, void(*move_direction)(int, int),
             pthread_mutex_lock(&elevator_lock);
         }
 
-        door_close(elevator);
           
         move_direction(elevator,elevator_direction);
         elevator_floor = at_floor+elevator_direction;
     }
 }
-#endif
